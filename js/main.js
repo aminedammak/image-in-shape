d0c29b3201a16d92db3eef609e302e82a021a55a
amine <aminedammak86@gmail.com>
2015-02-02 21:58:50 +0100

    Create an example that show how to do simple deformation of an image laterally

--- js/main.js
+++ js/main.js
@@ -1,24 +1,28 @@
 /*jslint vars: true, plusplus: true, devel: true, nomen: true, indent: 4, maxerr: 50 */
 /*global jQuery */
 
-//Function for importing in the carrousel's the images contained in the specified folder 
-
 (function ($) {
-    //Create circle
+	"use strict";
     var canvas = document.getElementById('canvas');
     var context = canvas.getContext("2d");
-    var centerX = canvas.width / 2;
-    var centerY = canvas.height / 2;
-    var radius = 70;
+    var imageObj = new Image(), sourceY = 0;
+	//Si on veut cropper l'image sans aucun effet de zoom il faut que sourceWidth soit égale à destWidth Le 4eme est le 8ieme
+	imageObj.src = 'img/sports-q-c-1024-768-5.jpg';
 
-      context.beginPath();
-      context.arc(centerX, centerY, radius, 0, 2 * Math.PI, false);
-      context.fillStyle = 'green';
-      context.fill();
-      context.lineWidth = 5;
-      context.strokeStyle = '#003300';
-      context.stroke();
-    
-    
-    
+	imageObj.onload = function () {
+		for (sourceY = 0; sourceY < 768; sourceY++) { console.log('ddd');
+			context.drawImage(imageObj, 0, sourceY, 1024 + sourceY, 1, 0, sourceY, 1024, 1);
+		}
+	};
+
+	
+	
+	
+	/*
+	imageObj.onload = function () {
+        context.drawImage(imageObj, 0, 0, 700, 600, 300, 400, 555, 777);
+		context.drawImage(imageObj, 0, 0, 700, 600, 0, 0, 555, 777);
+    };
+    */
+	
 }(jQuery));//End of musuem plugin global closure
