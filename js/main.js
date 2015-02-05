486d7f8e5132b3742127476827e66f14a2d0f6b0
amine <aminedammak86@gmail.com>
2015-02-05 22:05:14 +0100

    the animation of the deformed images work perfectly on chrome

--- js/main.js
+++ js/main.js
@@ -9,20 +9,40 @@
 	//Si on veut cropper l'image sans aucun effet de zoom il faut que sourceWidth soit égale à destWidth Le 4eme est le 8ieme
 	imageObj.src = 'img/sports-q-c-1024-768-5.jpg';
 
-	imageObj.onload = function () {
-		for (sourceY = 0; sourceY < 768; sourceY++) { console.log('ddd');
-			context.drawImage(imageObj, 0, sourceY, 1024 + sourceY, 1, 0, sourceY, 1024, 1);
-		}
-	};
+	window.requestAnimFrame = (function (callback) {
+		return window.requestAnimationFrame || window.webkitRequestAnimationFrame || window.mozRequestAnimationFrame || window.oRequestAnimationFrame || window.msRequestAnimationFrame ||
+			function (callback) {
+				window.setTimeout(callback, 1000 / 60);
+			};
+	}());
+	
+	function drawImage(time) {
+		for (sourceY = 0; sourceY < 768; sourceY++) { 
+				context.drawImage(imageObj, 0, sourceY, 1024 + sourceY, 1, 0, sourceY + time, 1024, 1);
+			}
+	}
 
-	
-	
-	
-	/*
-	imageObj.onload = function () {
-        context.drawImage(imageObj, 0, 0, 700, 600, 300, 400, 555, 777);
-		context.drawImage(imageObj, 0, 0, 700, 600, 0, 0, 555, 777);
-    };
-    */
+	function animate(startTime) {
+		// update
+        var time = (new Date()).getTime() - startTime;
+		console.log(time);
+		
+		var linearSpeed = 24890;
+        // pixels / second
+        var newX = linearSpeed * time / 1000;
+		
+		// clear
+        context.clearRect(0, 0, canvas.width, canvas.height);
+		
+		drawImage(newX);
+		
+		// request new frame
+        requestAnimFrame(function () {
+			animate(startTime);
+        });
+	}
+	var startTime = (new Date()).getTime();
+	animate(startTime);
+
 	
 }(jQuery));//End of musuem plugin global closure
