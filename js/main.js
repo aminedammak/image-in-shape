f208aaaa7ce25ed6a74a3071ad13c81f2f965a8c
amine <aminedammak86@gmail.com>
2015-02-21 17:06:59 +0100

    Make the thumbnail translate to the to left corner of the destination image

--- js/main.js
+++ js/main.js
@@ -17,7 +17,7 @@
 		getDestinationImage : function () {
 			return $(floppyCarousel.config.destinationImage).eq(0);
 		},
-		getThumbnails : function(){
+		getThumbnails : function () {
 			return $(floppyCarousel.config.thumbnails);
 		},
 		clickedTumbnail : {},
@@ -38,33 +38,59 @@
 			dimCord.height = height;
 			return dimCord;
 		},
+		xProgress : 0,
+		yProgress : 0,
+		getXYStep : function () {
+			var verticalDistance = floppyCarousel.getDimCord(floppyCarousel.getDestinationImage()).y -  floppyCarousel.getDimCord(floppyCarousel.clickedTumbnail).y;
+			var horizontalDistance = floppyCarousel.getDimCord(floppyCarousel.getDestinationImage()).x -  floppyCarousel.getDimCord(floppyCarousel.clickedTumbnail).x;
+			
+			var yRatio = Math.abs(verticalDistance) / Math.abs(horizontalDistance);
+			if (verticalDistance < 0) {
+				floppyCarousel.yProgress -= yRatio;
+			} else if (verticalDistance > 0) {
+				floppyCarousel.yProgress += yRatio;
+			}
+			
+			if (horizontalDistance < 0) {
+				floppyCarousel.xProgress -= 1;
+			} else if (horizontalDistance > 0) {
+				floppyCarousel.xProgress += 1;
+			}
+			return { xProgress : floppyCarousel.xProgress, yProgress : floppyCarousel.yProgress };
+		},
 		
 		animateImage : function (startTime) {
 			// update
 			var time = floppyCarousel.getActualTime() - startTime;
 
-			var linearSpeed = 190;
+			var linearSpeed = 390;
 			// pixels / second
-			var newX = linearSpeed * time / 10000;
-			var newY = (3 / 2) * newX;
+			var newX = linearSpeed * time / 1000;
 
 			// clear
 			floppyCarousel.canvasObj.context.clearRect(0, 0, floppyCarousel.canvasObj.canvas[0].width, floppyCarousel.canvasObj.canvas[0].height);
 
-			floppyCarousel.canvasObj.context.drawImage(floppyCarousel.canvasObj.imageObj, 0, 0, 1024, 768, floppyCarousel.getDimCord(floppyCarousel.clickedTumbnail).x, floppyCarousel.getDimCord(floppyCarousel.clickedTumbnail).y, floppyCarousel.canvasObj.imageObj.width, floppyCarousel.canvasObj.imageObj.height);
+			floppyCarousel.canvasObj.context.drawImage(
+				floppyCarousel.canvasObj.imageObj,
+				0,
+				0,
+				floppyCarousel.canvasObj.imageObj.width,
+				floppyCarousel.canvasObj.imageObj.height,
+				floppyCarousel.getDimCord(floppyCarousel.clickedTumbnail).x + floppyCarousel.getXYStep().xProgress,
+				floppyCarousel.getDimCord(floppyCarousel.clickedTumbnail).y + floppyCarousel.getXYStep().yProgress,
+				floppyCarousel.getDimCord(floppyCarousel.clickedTumbnail).width,
+				floppyCarousel.getDimCord(floppyCarousel.clickedTumbnail).height
+			);
 			// request new frame
 			window.requestAnimFrame(function () {
-							console.log(floppyCarousel.getDimCord(floppyCarousel.clickedTumbnail).y);
-
 				floppyCarousel.animateImage(startTime);
-							console.log("after"+floppyCarousel.getDimCord(floppyCarousel.clickedTumbnail).y);
-
+				console.log(floppyCarousel.getXYStep().yProgress);
 			});
 		
 		},
 		beginEffect : function () {
 			var that = this;
-				floppyCarousel.getThumbnails().click(function (e) {
+			floppyCarousel.getThumbnails().click(function (e) {
 				floppyCarousel.createIntermediateCanvas($(this));
 				var startTime = floppyCarousel.getActualTime();
 				floppyCarousel.clickedTumbnail = $(this);
